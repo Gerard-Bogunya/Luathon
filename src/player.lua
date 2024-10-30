d0c35b9269b7fdccbbf9cc025c70e27fc1e91d77
Gerard-Bogunya <97537561+Gerard-Bogunya@users.noreply.github.com>
2024-10-30 18:36:53 +0100

    movement

--- src/player.lua
+++ src/player.lua
@@ -2,24 +2,29 @@ local Actor = Actor or require "src/actor"
 local Vector = Vector or require "lib/vector"
 local Player = Actor:extend()
 
-function Player:new(_time, _repeat)    
-    self.speed = 100
+function Player:new()   
+  Player.super.new(self, "src/textures/p_blue_small.png", 180, 540, 0)
+    self.speed = 250
 end
 
 function Player:update(dt)
     
-  -- marquem direcció del vector
-  if love.keyboard.isDown("d") then 
-  
-    self.forward = Vector(1,0)
-  end
-  if love.keyboard.isDown("a") then 
+   -- Movimiento en el eje Y (arriba y abajo)
+   if love.keyboard.isDown("up") then
+    self.position.y = self.position.y - self.speed * dt
+elseif love.keyboard.isDown("down") then
+  self.position.y = self.position.y + self.speed * dt
+end
+
+-- Movimiento en el eje X (izquierda y derecha)
+if love.keyboard.isDown("left") then
+  self.position.x = self.position.x - self.speed * dt
+elseif love.keyboard.isDown("right") then
+  self.position.x = self.position.x + self.speed * dt
+self.forward = Vector(1,-1)
     
-    self.forward = Vector(-1,0)
   end
 
-  -- moviment de la nau
-  self.position = self.position + self.forward * self.speed * dt
 
 end
 
@@ -29,7 +34,8 @@ function Player:draw()
     local yy = self.position.y
     local oy = self.origin.y
     local rr = self.rot  
-    love.graphics.circle("fill", xx, yy, 3)
+    love.graphics.draw(self.image, xx, yy, rr, 1, 1, ox, oy)
+    
 end
 
 return Player
